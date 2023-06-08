d7a816fc5fc650eafeb31f97d1da27846761a5c1
HappybearDaBest <94869519+HappybearDaBest@users.noreply.github.com>
2023-06-08 16:03:21 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("clock").textContent = timeString;
+}
+
+setInterval(updateTime, 1000);
